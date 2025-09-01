53a0980d95dffa2da2d56094787353c10852f668
PBananarr <79418608+PBananarr@users.noreply.github.com>
2025-09-01 19:50:18 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'übergabe-cache-v2.1';
+const CACHE_NAME = 'übergabe-cache-v2.2';
 const ASSETS = [
   './',
   './index.html',
@@ -60,3 +60,4 @@ self.addEventListener('fetch', (event) => {
     }
   })());
 });
+
